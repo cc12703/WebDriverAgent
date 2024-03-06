8653153dc1fe9ceab25d2cc9191af20ce5b413f0
morris <cc12703@gmail.com>
2024-03-06 11:30:39 +0800

    fix  compile error of tvOS

--- relesae_ios.sh
+++ relesae_ios.sh
@@ -6,6 +6,6 @@ export DERIVED_DATA_PATH=out
 export SCHEME=WebDriverAgentRunner
 export DESTINATION=generic/platform=iOS
 export WD=out/Build/Products/Release-iphoneos
-export ZIP_PKG_NAME=WDA.ipa
+export ZIP_PKG_NAME=WDA-ios.ipa
 
 ./Scripts/ci/build-real.sh
---
+++ relesae_tvos.sh
@@ -0,0 +1,11 @@
+
+
+
+
+export DERIVED_DATA_PATH=out
+export SCHEME=WebDriverAgentRunner_tvOS
+export DESTINATION=generic/platform=tvOS
+export WD=out/Build/Products/Release-tvos
+export ZIP_PKG_NAME=WDA-tvos.ipa
+
+./Scripts/ci/build-real.sh
